81c7f9cd3dee2a2fab007ca59ba5f6131098bb67
Anthony Smith <420061+anthonator@users.noreply.github.com>
2020-02-14 12:08:26 -0500

    Provide function to making a request to Shopify

--- lib/shopify/oauth.ex
+++ lib/shopify/oauth.ex
@@ -1,9 +1,17 @@
 defmodule Shopify.OAuth do
-  alias Shopify.OAuth.{ Response }
+  alias Shopify.OAuth.{ Config, Operation, Request, Response }
 
   @type http_headers_t :: [{ String.t(), String.t() }]
 
   @type http_method_t :: :delete | :get | :post | :put
 
   @type response_t :: { :ok, Response.t() } | { :error, Response.t() | any }
+
+  @doc """
+  Send a request to Shopify.
+  """
+  @spec request(Operation.t(), Config.t()) :: response_t
+  def request(operation, config) do
+    Request.send(operation, Config.new(config))
+  end
 end
