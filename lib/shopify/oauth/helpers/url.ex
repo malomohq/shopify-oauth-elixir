1e3b5232a8690477220babb6fec82835a757da81
Anthony Smith <420061+anthonator@users.noreply.github.com>
2020-02-14 11:59:43 -0500

    Incorporiate operation path when building URL path

--- lib/shopify/oauth/helpers/url.ex
+++ lib/shopify/oauth/helpers/url.ex
@@ -1,21 +1,23 @@
 defmodule Shopify.OAuth.Helpers.URL do
   @moduledoc false
 
-  alias Shopify.OAuth.{ Config }
+  alias Shopify.OAuth.{ Config, Operation }
 
-  @spec to_string(Config.t()) :: String.t()
-  def to_string(config) do
-    config
-    |> to_uri()
+  @spec to_string(Operation.t(), Config.t()) :: String.t()
+  def to_string(operation, config) do
+    operation
+    |> to_uri(config)
     |> URI.to_string()
   end
 
-  @spec to_uri(Config.t()) :: URI.t()
-  def to_uri(config) do
-    uri = %URI{ path: config.path, port: config.port, scheme: config.protocol }
-    uri = put_host(uri, config)
-
-    uri
+  @spec to_uri(Operation.t(), Config.t()) :: URI.t()
+  def to_uri(operation, config) do
+    %URI{}
+    |> Map.put(:path, "#{config.path}#{operation.path}")
+    |> Map.put(:port, config.port)
+    |> Map.put(:scheme, config.protocol)
+    |> put_host(config)
+    |> put_query(operation)
   end
 
   defp put_host(uri, %{ shop: shop } = config) when not is_nil(shop) do
@@ -24,5 +26,13 @@ defmodule Shopify.OAuth.Helpers.URL do
 
   defp put_host(uri, config) do
     Map.put(uri, :host, config.host)
+  end
+
+  defp put_query(uri, %{ method: :get } = operation) do
+    Map.put(uri, :query, URI.encode_query(operation.params))
+  end
+
+  defp put_query(uri, _operation) do
+    uri
   end
 end
--- test/shopify/oauth/helpers/url_test.exs
+++ test/shopify/oauth/helpers/url_test.exs
@@ -1,19 +1,23 @@
 defmodule Shopify.OAuth.Helpers.URLTest do
   use ExUnit.Case, async: true
 
-  alias Shopify.OAuth.{ Config, Helpers }
+  alias Shopify.OAuth.{ Config, Helpers, Operation }
 
   describe "to_string/1" do
     test "with :shop" do
       config = Config.new(%{ shop: "a-shop" })
 
-      assert Helpers.URL.to_string(config) == "#{config.protocol}://a-shop.#{config.host}#{config.path}"
+      operation = %Operation{ method: :post, path: "/a-path" }
+
+      assert Helpers.URL.to_string(operation, config) == "#{config.protocol}://a-shop.#{config.host}#{config.path}#{operation.path}"
     end
 
     test "without :shop" do
       config = Config.new()
 
-      assert Helpers.URL.to_string(config) == "#{config.protocol}://#{config.host}#{config.path}"
+      operation = %Operation{ method: :post, path: "/a-path" }
+
+      assert Helpers.URL.to_string(operation, config) == "#{config.protocol}://#{config.host}#{config.path}#{operation.path}"
     end
   end
 
@@ -23,21 +27,25 @@ defmodule Shopify.OAuth.Helpers.URLTest do
 
       config = Config.new(%{ shop: shop })
 
+      operation = %Operation{ method: :post, path: "/a-path" }
+
       host = "#{shop}.#{config.host}"
-      path = config.path
+      path = "#{config.path}#{operation.path}"
       scheme = config.protocol
 
-      assert %URI{ host: ^host, path: ^path, scheme: ^scheme } = Helpers.URL.to_uri(config)
+      assert %URI{ host: ^host, path: ^path, scheme: ^scheme } = Helpers.URL.to_uri(operation, config)
     end
 
     test "without :shop" do
       config = Config.new()
 
+      operation = %Operation{ method: :post, path: "/a-path" }
+
       host = "#{config.host}"
-      path = config.path
+      path = "#{config.path}#{operation.path}"
       scheme = config.protocol
 
-      assert %URI{ host: ^host, path: ^path, scheme: ^scheme } = Helpers.URL.to_uri(config)
+      assert %URI{ host: ^host, path: ^path, scheme: ^scheme } = Helpers.URL.to_uri(operation, config)
     end
   end
 end
