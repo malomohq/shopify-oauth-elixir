55926ec96b3f90cd55a0dd47877d16c2fe53ce23
Anthony Smith <420061+anthonator@users.noreply.github.com>
2020-02-14 10:58:08 -0500

    Minor clean up

--- lib/shopify/oauth/client.ex
+++ lib/shopify/oauth/client.ex
@@ -6,14 +6,14 @@ defmodule Shopify.OAuth.Client do
   @type response_t ::
           %{
             body: binary,
-            headers: Shopify.OAuth.http_headers_t,
+            headers: Shopify.OAuth.http_headers_t(),
             status_code: pos_integer
           }
 
   @callback request(
-              method :: Shopify.OAuth.http_method_t,
+              method :: Shopify.OAuth.http_method_t(),
               url :: String.t(),
-              headers :: Shopify.OAuth.http_headers_t,
+              headers :: Shopify.OAuth.http_headers_t(),
               body :: binary,
               opts :: any
             ) :: { :ok, response_t() } | { :error, any }
