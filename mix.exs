8fb0ed6d82d1fc7e052b230346171be80835ea1a
Anthony Smith <420061+anthonator@users.noreply.github.com>
2020-02-14 10:18:21 -0500

    :1st_place_medal:

---
+++ mix.exs
@@ -0,0 +1,23 @@
+defmodule Shopify.OAuth.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :shopify_oauth,
+      version: "0.0.0",
+      elixir: "~> 1.8",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  def application do
+    [
+      extra_applications: [:logger]
+    ]
+  end
+
+  defp deps do
+    []
+  end
+end
