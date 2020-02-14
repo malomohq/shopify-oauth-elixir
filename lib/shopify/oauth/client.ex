61bccdcade363ddc2f1cf59bc428f5c8fbc7ca5e
Anthony Smith <420061+anthonator@users.noreply.github.com>
2020-02-14 10:47:00 -0500

    Added HTTP client behaviour

---
+++ lib/shopify/oauth/client.ex
@@ -0,0 +1,20 @@
+defmodule Shopify.OAuth.Client do
+  @moduledoc """
+  Behaviour for implementing an HTTP client.
+  """
+
+  @type response_t ::
+          %{
+            body: binary,
+            headers: Shopify.OAuth.http_headers_t,
+            status_code: pos_integer
+          }
+
+  @callback request(
+              method :: Shopify.OAuth.http_method_t,
+              url :: String.t(),
+              headers :: Shopify.OAuth.http_headers_t,
+              body :: binary,
+              opts :: any
+            ) :: { :ok, response_t() } | { :error, any }
+end
