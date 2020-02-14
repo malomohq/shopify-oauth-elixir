7714ce7ebd0fd533076ea24ebdae60db8b369028
Anthony Smith <420061+anthonator@users.noreply.github.com>
2020-02-14 12:06:05 -0500

    Added type response_t

--- lib/shopify/oauth.ex
+++ lib/shopify/oauth.ex
@@ -1,5 +1,9 @@
 defmodule Shopify.OAuth do
+  alias Shopify.OAuth.{ Response }
+
   @type http_headers_t :: [{ String.t(), String.t() }]
 
   @type http_method_t :: :delete | :get | :post | :put
+
+  @type response_t :: { :ok, Response.t() } | { :error, Response.t() | any }
 end
