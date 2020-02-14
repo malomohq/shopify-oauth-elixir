2cdcac3ba020733917d798c4b785564263401ea9
Anthony Smith <420061+anthonator@users.noreply.github.com>
2020-02-14 11:33:03 -0500

    Created operation struct

---
+++ lib/shopify/oauth/operation.ex
@@ -0,0 +1,10 @@
+defmodule Shopify.OAuth.Operation do
+  @type t ::
+          %__MODULE__{
+            method: Shopify.OAuth.http_method_t(),
+            params: map,
+            path: String.t()
+          }
+
+  defstruct method: nil, pararms: %{}, path: nil
+end
