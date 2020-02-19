2c0e408a40f729576498d11f1cfa23b1fa75f78a
Anthony Smith <420061+anthonator@users.noreply.github.com>
2020-02-19 13:40:34 -0500

    Bump version to 1.0.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Shopify.OAuth.MixProject do
   def project do
     [
       app: :shopify_oauth,
-      version: "1.0.0",
+      version: "1.0.1",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
